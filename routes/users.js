bdbe6ed6501836079bb3812882119a634cabc908
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-02-26 21:51:56 -0500

    Feat: implement user routes

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,65 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into /users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
 
-const express = require('express');
-const router  = express.Router();
+const bcrypt = require('bcrypt');
 
-router.get('/', (req, res) => {
-  res.render('users');
-}); 
+module.exports = function(router, database) {
 
-module.exports = router;
+  // Create a new user
+  router.post('/', (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 12);
+    database.addUser(user)
+    .then(user => {
+  
+      //TODO: revisit handling this error
+      if (!user) {
+        res
+          .status(500) 
+          .send("User creation unsuccessful");
+        return;
+      }
+      req.session.userId = user.id;
+      res.send("User successfully created!");
+    })
+    .catch(e => res.send(e));
+  });
+
+  /**
+   * Check if a user exists with a given username and password
+   * @param {String} email
+   * @param {String} password encrypted
+   */
+  const login =  function(email, password) {
+    return database.getUserWithEmail(email)
+    .then(user => {
+      if (bcrypt.compareSync(password, user.password)) {
+        return user;
+      }
+      return null;
+    });
+  }
+  exports.login = login;
+
+  router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    login(email, password)
+      .then(user => {
+        if (!user) {
+          res
+            .status(404)
+            .send("User doesn't exist!");
+          return;
+        }
+        req.session.userId = user.id;
+        res.send({user: {name: user.name, email: user.email, id: user.id}});
+      })
+      .catch(e => res.send(e));
+  });
+  
+  router.post('/logout', (req, res) => {
+    req.session.userId = null;
+    res.send({});
+  });
+
+  return router;
+
+}
