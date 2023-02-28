46b612e4692ee88e7ae332e1b62b1194a18464b2
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-02-28 13:53:31 -0500

    Feat: redirect on cookie set

--- server.js
+++ server.js
@@ -114,12 +114,14 @@ app.get('/about', (req, res) => {
 app.post('/login/1', (req, res) => {
 
   res.cookie('user', 1);
+  res.redirect('/');
 
 });
 
 app.post('/login/2', (req, res) => {
 
   res.cookie('user', 2);
+  res.redirect('/');
 
 });
 
