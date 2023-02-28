0cb021c2392932026b2dff8af1b4a6da3af62eaa
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-02-28 15:20:37 -0500

    Refactor: order_items property name

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -48,5 +48,5 @@ CREATE TABLE orders (
 CREATE TABLE order_items (
   id SERIAL PRIMARY KEY NOT NULL,
   order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
-  menu_items_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE
+  menu_item_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE
 );
