5e979dc85a3d18453d22c0f0467c08dcaa1ff505
penguinboots <sabrina.ynw@gmail.com>
2023-03-03 11:56:54 -0800

    add cart rendering scripts

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,39 @@
 // Client facing scripts here
+
+$(document).ready(() => {
+  // create menu article element given raw menu data
+  const createCartElement = function(cartItem) {
+    const menuElement = $(`
+      <article class="cart-item">
+        <div>PLACEHOLDER ITEM</div>
+      </article>
+    `);
+    return menuElement;
+  };
+
+  // given array of tweets, append generated article element to tweets-container
+  const renderCart = function(cartItems) {
+    const $container = $(".cart-items");
+    $container.empty();
+    for (const item of cartItems) {
+      const oneItem = createCartElement(item);
+      $container.append(oneItem);
+    }
+  };
+
+  const loadCart = function() {
+    $.ajax({
+      url: '/cart',
+      method: 'GET',
+      success: (cart) => {
+        renderCart(cart);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  };
+
+  loadCart();
+
+});
