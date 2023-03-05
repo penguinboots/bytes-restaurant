3280978cb6265af022f3108bc5b1173db44081ff
mzparulina <pauline.revilla@gmail.com>
2023-03-05 01:08:46 -0500

    Fix merge conflicts

--- db/queries/cart_items.js
+++ db/queries/cart_items.js
@@ -6,11 +6,20 @@ Utils.isObject = x => x !== null && typeof x === "object";
 Utils.isObjEmpty = obj => Utils.isObject(obj) && Object.keys(obj).length === 0;
 
 // GET /orders/menu
-const getCartItembyUserID = (userID) => {
+const getCartItemsbyUserID = (userID) => {
   const queryString = `
-    SELECT cart_items.id, name, price, quantity FROM cart_items join menu_items on menu_items.id = item_id where customer_id = $1;
+    SELECT cart_items.id, name, price, quantity, item_id FROM cart_items join menu_items on menu_items.id = item_id where user_id = $1;
   `;
   const values = [userID];
+  return db.query(queryString, values)
+    .then(data => data.rows);
+};
+
+const getQuantityInCart = (userID, itemID) => {
+  const queryString = `
+    SELECT quantity FROM cart_items join menu_items on menu_items.id = item_id WHERE user_id = $1 AND item_id = $2;
+  `;
+  const values = [userID, itemID];
   return db.query(queryString, values)
     .then(data => data.rows);
 };
@@ -68,9 +77,6 @@ const updateCartItems = (conditions, data) => {
     .then(data => data.rows[0]);
 };
 
-<<<<<<< Updated upstream
-module.exports = { getCartItembyUserID, createCartItem, updateCartItems };
-=======
 const updateCartQuantities = (params) => {
   const dataTuples = params.map((k) => `(${k.quantity}, ${k.item_id}, ${k.user_id})`);
   const val = dataTuples.join(',');
@@ -82,7 +88,6 @@ const updateCartQuantities = (params) => {
     where c.item_id = c1.item_id and c1.user_id = c.user_id`;
   return db.query(queryString)
     .then(data => data.rows);
-}
+};
 
 module.exports = { getCartItemsbyUserID, createCartItem, updateCartItems, getQuantityInCart, updateCartQuantities };
->>>>>>> Stashed changes
