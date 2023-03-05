0e103b8913ed06bb7bc6ddf97f2c518069a3cec3
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-03-05 15:57:21 -0500

    Chore: stash changes

--- routes/users.js
+++ routes/users.js
@@ -110,45 +110,52 @@ module.exports = function(router, database) {
 
   });
 
-  // Get user orders
-  router.post('/orders', (req, res) => {
-    const username = req.cookies["userId"];
+  // // Create an order
+  // router.post('/orders', async (req, res) => {
+  //   const username = req.cookies["userId"];
 
-    // if (!username) {
-    //   res
-    //     .status(401)
-    //     .send("No currently logged in user detected");
-    //   return;
-    // }
+  //   // if (!username) {
+  //   //   res
+  //   //     .status(401)
+  //   //     .send("No currently logged in user detected");
+  //   //   return;
+  //   // }
 
-    database.getAllUserOrders(username)
-      .then(order => res.send(order))
-      .catch(e => {
-        console.error(e);
-        res.send(e);
-      });
+  //   try {
 
-  });
+  //     let cart = await database.getCartItemsbyUserID(req.cookies["userId"]);
 
-  // Create an order
-  router.post('/orders', (req, res) => {
-    const username = req.cookies["userId"];
+  //     //* Calculating cart total (see if val can be passed from FE)
+  //     const total = cart.reduce((accumulator, val["total"]) => accumulator + val["total"], 0);
+  //     console.log(total);
+  //     res.send(`total: ${total}`);
+  //     return;
 
-    // if (!username) {
-    //   res
-    //     .status(401)
-    //     .send("No currently logged in user detected");
-    //   return;
-    // }
+  //     //* Create an order retrieving it's order id (order placed status)
 
-    database.addOrder({ ...req.body })
-      .then(order => res.send(order))
-      .catch(e => {
-        console.error(e);
-        res.send(e);
-      });
+  //     await 
 
-  });
+  //     // Send text to resto about order
+
+  //     //* Using the order id, insert the cart items into order_items
+
+  //     // Purge the cart
+
+  //       database.addOrder({ ...req.body })
+  //         .then(order => res.send(order))
+  //         .catch(e => {
+  //           console.error(e);
+  //           res.send(e);
+  //         });
+
+  //   } catch (err) {
+
+  //     console.error(err);
+  //     res.status(500);
+
+  //   }
+
+  // });
 
   // Get user orders
   router.get('/orders', (req, res) => {
