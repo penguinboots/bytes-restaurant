b6b12fa19acef78048a12ae20e7f823d56a6d07a
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-03-05 02:20:10 -0500

    Feat: implement cart item modification Cart item increment, decrement and removal

--- routes/combined-api.js
+++ routes/combined-api.js
@@ -111,7 +111,7 @@ module.exports = function(router, database) {
       const cart = await database.getCartItemsbyUserID(req.cookies["userId"]);
 
       const promises = cart.map(cart_item => {
-        return database.updateCartItems({user_id: req.cookies["userId"], item_id: cart_item.item_id}, {quantity: 0});
+        return database.updateCartItems({ user_id: req.cookies["userId"], item_id: cart_item.item_id }, { quantity: 0 });
       });
 
       await Promise.all(promises);
@@ -123,27 +123,111 @@ module.exports = function(router, database) {
     }
   });
 
-  // // Increment cart item
-  // router.post('/cart/increase', async (req, res) => {
+  // Increment cart item
+  router.post('/cart/increase', async (req, res) => {
 
-  //   try {
+    try {
 
-  //     const item_id = req.body["cartItemId"];
-  //     let quantity = await database.getQuantityInCart(req.cookies["userId"], item_id);
-  //     console.log(quantity);
-  //     ++quantity["quantity"];
-  //     await database.updateCartItems({ user_id: req.cookies["userId"], item_id: item_id, }, { quantity: quantity });
-  //     res.status(200);
-  //     return;
+      const item_id = req.body["cartItemId"];
+      let menu_id;
+      let quantity;
 
-  //   } catch (err) {
+      // Try to get the menu item id to streamline this process
+      const cart_items = await database.getCartItemsbyUserID(req.cookies["userId"]);
 
-  //     console.error(err)
-  //     res.status(500);
+      console.log(cart_items);
 
-  //   }
+      for (let item of cart_items) {
+        if (item["id"] === Number(item_id)) {
+          quantity = item["quantity"];
+          menu_id = item["item_id"];
+        }
+      }
 
-  // });
+      quantity++;
+      await database.updateCartItems({ user_id: req.cookies["userId"], item_id: menu_id, }, { quantity: quantity });
+
+      res.status(200).redirect('back');
+      return;
+
+    } catch (err) {
+
+      console.error(err);
+      res.status(500);
+
+    }
+
+  });
+
+   // Decrement cart item
+   router.post('/cart/decrease', async (req, res) => {
+
+    try {
+
+      const item_id = req.body["cartItemId"];
+      let menu_id;
+      let quantity;
+
+      // Try to get the menu item id to streamline this process
+      const cart_items = await database.getCartItemsbyUserID(req.cookies["userId"]);
+
+      console.log(cart_items);
+
+      for (let item of cart_items) {
+        if (item["id"] === Number(item_id)) {
+          quantity = item["quantity"];
+          menu_id = item["item_id"];
+        }
+      }
+
+      quantity--;
+      await database.updateCartItems({ user_id: req.cookies["userId"], item_id: menu_id, }, { quantity: quantity });
+
+      res.status(200).redirect('back');
+      return;
+
+    } catch (err) {
+
+      console.error(err);
+      res.status(500);
+
+    }
+
+  });
+
+   // Remove cart item
+   router.post('/cart/remove', async (req, res) => {
+
+    try {
+
+      const item_id = req.body["cartItemId"];
+      let menu_id;
+      let quantity;
+
+      // Try to get the menu item id to streamline this process
+      const cart_items = await database.getCartItemsbyUserID(req.cookies["userId"]);
+
+      console.log(cart_items);
+
+      for (let item of cart_items) {
+        if (item["id"] === Number(item_id)) {
+          menu_id = item["item_id"];
+        }
+      }
+
+      await database.updateCartItems({ user_id: req.cookies["userId"], item_id: menu_id, }, { quantity: 0 });
+
+      res.status(200).redirect('back');
+      return;
+
+    } catch (err) {
+
+      console.error(err);
+      res.status(500);
+
+    }
+
+  });
 
   return router;
 
